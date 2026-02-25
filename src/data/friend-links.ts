6c5ebb0c84bdb1c34c6d8bf523cbb1cb18c15e46
EnderWave <3318725998@qq.com>
2026-02-25 17:47:05 +0800

    upd new friends

--- src/data/friend-links.ts
+++ src/data/friend-links.ts
@@ -3,6 +3,7 @@ export type FriendLinkItem = {
 	url: string;
 	avatar: string;
 	desc: string;
+	email: string;
 };
 
 export type FriendLinkSection = {
@@ -15,10 +16,18 @@ export const friendLinkSections: FriendLinkSection[] = [
 		title: "🌟 友情博客",
 		items: [
 			{
-				title: "博客园",
-				url: "https://www.cnblogs.com/ZYPLJ/",
-				avatar: "https://pljzy.top/images/logo4.jpg",
-				desc: "技术分享与学习交流的园地",
+				title: "Farewe1ll's Blog",
+				url: "https://blog.farewe1ll.com",
+				avatar: "https://avatar.farewe1ll.top",
+				desc: "撥雪尋春，燒燈續晝。",
+				email: "me@Farewe1ll.com",
+			},
+			{
+				title: "Yukii_P's Blog",
+				url: "https://blog.yukiip.top/",
+				avatar: "https://s2.loli.net/2024/02/19/aZ9KP67BJNUWLgC.jpg",
+				desc: "博闻善思",
+				email: "",
 			},
 		],
 	},
